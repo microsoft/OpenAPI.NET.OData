fe608056a4ee7fac0ed527c8be68bf4e834e217c
Sam Xu <saxu@microsoft.com>
2019-10-03 16:35:05 -0700

    Enable auto-generate the Version from Custom build task

--- src/AssemblyInfo/AssemblyInfoCommon.cs
+++ src/AssemblyInfo/AssemblyInfoCommon.cs
@@ -12,20 +12,24 @@ using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using System.Security;
 
-// [assembly: AssemblyCompany("Microsoft Corporation")]
 // If you want to control this metadata globally but not with the VersionProductName property, hard-code the value below.
 // If you want to control this metadata at the individual project level with AssemblyInfo.cs, comment-out the line below.
 // If you leave the line below unchanged, make sure to set the property in the root build.props, e.g.: <VersionProductName Condition="'$(VersionProductName)'==''">Your Product Name</VersionProductName>
 // [assembly: AssemblyProduct("%VersionProductName%")]
+
+[assembly: AssemblyCompany("Microsoft Corporation")]
+
+[assembly: AssemblyProduct("Microsoft® .NET/.NET Core OData Extensions")]
+
 [assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation. All rights reserved.")]
+
 [assembly: AssemblyTrademark("Microsoft and Windows are either registered trademarks or trademarks of Microsoft Corporation in the U.S. and/or other countries.")]
+
 [assembly: AssemblyCulture("")]
 
-/*
 #if (DEBUG || _DEBUG)
 [assembly: AssemblyConfiguration("Debug")]
 #endif
-*/
 
 // disable CLSCompliant as the common extensions library decided not to support this
 // see https://github.com/aspnet/AspNetCore/issues/2689#issuecomment-354693946
