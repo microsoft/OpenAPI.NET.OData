f17df6794fb3409a0c9a7003c7a83caf9c3dce31
Irvine Sunday <v-irsund@microsoft.com>
2020-03-27 14:04:05 +0300

    Helper class with method that gets the schema for derived types of $refs

---
+++ src/Microsoft.OpenApi.OData.Reader/Common/Helpers.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.OData.Edm;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.OData.Edm;
+
+namespace Microsoft.OpenApi.OData.Common
+{
+    public static class Helpers
+    {
+        /// <summary>
+        /// Adds the derived types references together with their base type reference in the OneOf property of an OpenAPI schema.
+        /// </summary>
+        /// <returns>The OpenAPI schema with the list of derived types references and their base type references set in the OneOf property.</returns>
+        internal static OpenApiSchema GetDerivedTypesReferenceSchema(IEdmEntityType entityType, ODataContext context)
+        {
+            IEnumerable<IEdmEntityType> derivedTypes = context.Model.FindDirectlyDerivedTypes(entityType).OfType<IEdmEntityType>();
+
+            if (!derivedTypes.Any())
+            {
+                return null;
+            }
+
+            OpenApiSchema schema = new OpenApiSchema
+            {
+                OneOf = new List<OpenApiSchema>()
+            };
+
+            OpenApiSchema baseTypeSchema = new OpenApiSchema
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.Schema,
+                    Id = entityType.FullName()
+                }
+            };
+            schema.OneOf.Add(baseTypeSchema);
+
+            foreach (IEdmEntityType derivedType in derivedTypes)
+            {
+                OpenApiSchema derivedTypeSchema = new OpenApiSchema
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.Schema,
+                        Id = derivedType.FullName()
+                    }
+                };
+                schema.OneOf.Add(derivedTypeSchema);
+            };
+
+            return schema;
+        }
+    }
+}
