7bf3ce6a5f3bb7df4226cc4d4fcfdcf8b0d5f044
Irvine Sunday <v-irsund@microsoft.com>
2020-03-30 16:37:18 +0300

    Test to validate fetching of derived types of base type into schema

---
+++ test/Microsoft.OpenAPI.OData.Reader.Tests/Common/HelpersTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.OData.Edm;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.OData.Edm;
+using Microsoft.OpenApi.OData.Tests;
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.OpenApi.OData.Common.Tests
+{
+    public class HelpersTests
+    {
+        [Fact]
+        public void GetDerivedTypesReferenceSchemaReturnsDerivedTypesReferencesInSchemaIfExist()
+        {
+            // Arrange
+            IEdmModel edmModel = EdmModelHelper.GraphBetaModel;
+            ODataContext context = new ODataContext(edmModel);
+            IEdmEntityType entityType = edmModel.SchemaElements.OfType<IEdmEntityType>().First(c => c.Name == "directoryObject");
+            OpenApiSchema schema = null;
+
+            // Act
+            schema = Helpers.GetDerivedTypesReferenceSchema(entityType, context.Model);
+
+            // Assert
+            Assert.NotNull(schema.OneOf);
+            Assert.Equal(16, schema.OneOf.Count);
+        }
+
+        [Fact]
+        public void GetDerivedTypesReferenceSchemaReturnsNullSchemaIfNotExist()
+        {
+            // Arrange
+            IEdmModel model = EdmModelHelper.GraphBetaModel;
+            ODataContext context = new ODataContext(model);
+            IEdmEntityType entityType = model.SchemaElements.OfType<IEdmEntityType>().First(c => c.Name == "administrativeUnit");
+            OpenApiSchema schema = null;
+
+            // Act
+            schema = Helpers.GetDerivedTypesReferenceSchema(entityType, context.Model);
+
+            // Assert
+            Assert.Null(schema);
+        }
+    }
+}
