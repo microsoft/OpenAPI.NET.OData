e6a5e52e113ada17088422611e5e45761914147d
Irvine Sunday <40403681+irvinesunday@users.noreply.github.com>
2021-08-18 16:33:26 +0300

    [Fix] Generate paths for stream properties in base types of entities (#110)
    * Get all properties declared in type def. including base types
    
    * Update/refactor tests to validate stream props. of base types are captured
    
    * Refactor Handler class to use encapsulation to define navigation sources

--- test/Microsoft.OpenAPI.OData.Reader.Tests/Operation/MediaEntityGetOperationHandlerTests.cs
+++ test/Microsoft.OpenAPI.OData.Reader.Tests/Operation/MediaEntityGetOperationHandlerTests.cs
@@ -55,13 +55,13 @@ namespace Microsoft.OpenApi.OData.Operation.Tests
             Assert.NotNull(me);
 
             IEdmEntityType todo = model.SchemaElements.OfType<IEdmEntityType>().First(c => c.Name == "Todo");
-            IEdmStructuralProperty sp = todo.DeclaredStructuralProperties().First(c => c.Name == "Logo");
+            IEdmStructuralProperty sp = todo.StructuralProperties().First(c => c.Name == "Logo");
             ODataPath path = new ODataPath(new ODataNavigationSourceSegment(todos),
                 new ODataKeySegment(todos.EntityType()),
                 new ODataStreamPropertySegment(sp.Name));
 
             IEdmEntityType user = model.SchemaElements.OfType<IEdmEntityType>().First(c => c.Name == "user");
-            IEdmNavigationProperty navProperty = user.DeclaredNavigationProperties().First(c => c.Name == "photo");
+            IEdmNavigationProperty navProperty = user.NavigationProperties().First(c => c.Name == "photo");
             ODataPath path2 = new ODataPath(new ODataNavigationSourceSegment(me),
                 new ODataNavigationPropertySegment(navProperty),
                 new ODataStreamContentSegment());
