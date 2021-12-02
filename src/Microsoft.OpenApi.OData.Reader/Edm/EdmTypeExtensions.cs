5b59e12dda241ba95291cff7ebb3dde59caa767a
Vincent Biret <vibiret@microsoft.com>
2021-12-02 12:24:37 -0500

    - replaces tabs by spaces
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Microsoft.OpenApi.OData.Reader/Edm/EdmTypeExtensions.cs
+++ src/Microsoft.OpenApi.OData.Reader/Edm/EdmTypeExtensions.cs
@@ -7,29 +7,29 @@ using Microsoft.OData.Edm;
 
 namespace Microsoft.OpenApi.OData.Edm
 {
-	/// <summary>
+    /// <summary>
     /// Extension methods for <see cref="IEdmType"/>
     /// </summary>
     public static class EdmTypeExtensions
-	{
-		/// <summary>
-		/// Determines wether a path parameter should be wrapped in quotes based on the type of the parameter.
-		/// </summary>
-		/// <param name="edmType">The type of the parameter.</param>
-		public static bool ShouldPathParameterBeQuoted(this IEdmType edmType)
-		{
-			if (edmType == null)
-			{
-				return false;
-			}
+    {
+        /// <summary>
+        /// Determines wether a path parameter should be wrapped in quotes based on the type of the parameter.
+        /// </summary>
+        /// <param name="edmType">The type of the parameter.</param>
+        public static bool ShouldPathParameterBeQuoted(this IEdmType edmType)
+        {
+            if (edmType == null)
+            {
+                return false;
+            }
 
-			return edmType.TypeKind switch
-			{
-				EdmTypeKind.Enum => true,
-				EdmTypeKind.Primitive when edmType.IsString() || edmType.IsTemporal() => true,
-				_ => false,
-			};
-		}
-	}
+            return edmType.TypeKind switch
+            {
+                EdmTypeKind.Enum => true,
+                EdmTypeKind.Primitive when edmType.IsString() || edmType.IsTemporal() => true,
+                _ => false,
+            };
+        }
+    }
 
 }
