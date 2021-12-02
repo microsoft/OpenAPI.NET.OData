299bfc9aa27cf011a154cacba195d8ea3e5a3de3
Vincent Biret <vibiret@microsoft.com>
2021-12-02 14:39:14 -0500

    - adds a unit test for the id under navigation property case

--- test/Microsoft.OpenAPI.OData.Reader.Tests/Operation/ODataTypeCastGetOperationHandlerTests.cs
+++ test/Microsoft.OpenAPI.OData.Reader.Tests/Operation/ODataTypeCastGetOperationHandlerTests.cs
@@ -78,6 +78,64 @@ public class ODataTypeCastGetOperationHandlerTests
   [InlineData(true, false)]
   [InlineData(false, true)]
   [InlineData(false, false)]
+  public void CreateODataTypeCastGetOperationReturnsCorrectOperationForCollectionNavigationPropertyId(bool enableOperationId, bool enablePagination)
+  {// ../People/{id}/Friends/{id}/Microsoft.OData.Service.Sample.TrippinInMemory.Models.Employee/
+    // Arrange
+    IEdmModel model = EdmModelHelper.TripServiceModel;
+    OpenApiConvertSettings settings = new()
+    {
+        EnableOperationId = enableOperationId,
+        EnablePagination = enablePagination,
+    };
+    ODataContext context = new(model, settings);
+    IEdmEntitySet people = model.EntityContainer.FindEntitySet("People");
+    Assert.NotNull(people);
+
+    IEdmEntityType person = model.SchemaElements.OfType<IEdmEntityType>().First(c => c.Name == "Person");
+    IEdmEntityType employee = model.SchemaElements.OfType<IEdmEntityType>().First(c => c.Name == "Employee");
+    IEdmNavigationProperty navProperty = person.DeclaredNavigationProperties().First(c => c.Name == "Friends");
+    ODataPath path = new(new ODataNavigationSourceSegment(people),
+                                  new ODataKeySegment(people.EntityType()),
+                                  new ODataNavigationPropertySegment(navProperty),
+                                  new ODataKeySegment(people.EntityType()),
+                                  new ODataTypeCastSegment(employee));
+
+    // Act
+    var operation = _operationHandler.CreateOperation(context, path);
+
+    // Assert
+    Assert.NotNull(operation);
+    Assert.Equal("Get the item of type Microsoft.OData.Service.Sample.TrippinInMemory.Models.Person as Microsoft.OData.Service.Sample.TrippinInMemory.Models.Employee", operation.Summary);
+    Assert.NotNull(operation.Tags);
+    var tag = Assert.Single(operation.Tags);
+    Assert.Equal("Person.Employee", tag.Name);
+    Assert.Empty(tag.Extensions);
+
+    Assert.NotNull(operation.Parameters);
+    Assert.Equal(4, operation.Parameters.Count); //select, expand, id, id
+
+    Assert.Null(operation.RequestBody);
+    if(enablePagination)
+      Assert.Empty(operation.Extensions);
+
+    Assert.Equal(2, operation.Responses.Count);
+    Assert.Equal(new string[] { "200", "default" }, operation.Responses.Select(e => e.Key));
+
+    if (enableOperationId)
+    {
+      Assert.Equal("Get.Microsoft.OData.Service.Sample.TrippinInMemory.Models.Person.As.Microsoft.OData.Service.Sample.TrippinInMemory.Models.Employee", operation.OperationId);
+    }
+    else
+    {
+      Assert.Null(operation.OperationId);
+    }
+    Assert.False(operation.Responses["200"].Content["application/json"].Schema.Properties.ContainsKey("value"));
+  }
+  [Theory]
+  [InlineData(true, true)]
+  [InlineData(true, false)]
+  [InlineData(false, true)]
+  [InlineData(false, false)]
   public void CreateODataTypeCastGetOperationReturnsCorrectOperationForEntitySet(bool enableOperationId, bool enablePagination)
   {// .../People/Microsoft.OData.Service.Sample.TrippinInMemory.Models.Employee
     // Arrange
