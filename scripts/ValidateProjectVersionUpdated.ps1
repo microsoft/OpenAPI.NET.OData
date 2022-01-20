330231f4eb9cb42dbd65b73685111c8777bd78be
Daniel Mbaluka <danielmbaluka@gmail.com>
2022-01-20 13:46:54 +0300

    validate if the project version has been bumped in the pipeline

--- scripts/ValidateProjectVersionUpdated.ps1
+++ scripts/ValidateProjectVersionUpdated.ps1
@@ -9,33 +9,35 @@
     specified in the project file with the latest package version published on
     NuGet. If the version has not been updated, the script will fail and indicate
     that the project version neeeds to be updated.
+
+.Parameter projectVersion
+    Specifies the current project version. The project version currently is maintained 
+    in the versioning.props file
 #>
 
-$versionProps = ".\tool\versioning.props"
+Param(
+	[parameter(Mandatory = $true)]
+	[string]$projectVersion
+)
+
 $packageName = "Microsoft.OpenApi.OData"
 
-[xml]$xmlDoc = Get-Content $versionProps
-
-# Get the project major, minor and patch version numbers
-$majorVersion = $xmlDoc.Project.PropertyGroup.VersionMajor.InnerText
-$minorVersion = $xmlDoc.Project.PropertyGroup.VersionMinor.InnerText
-$patchVersion = $xmlDoc.Project.PropertyGroup.VersionBuild.InnerText
-
-[int]$currentProjectVersion = $majorVersion + $minorVersion + $patchVersion
+# Convert the project semantic version into a number e.g. "1.0.9" -> 109
+[int]$currentProjectVersionNumber = $projectVersion -replace "\."
 
 # API is case-sensitive
 $packageName = $packageName.ToLower()
-$url = "https://api.nuget.org/v3/registration3/$packageName/index.json"
+$url = "https://api.nuget.org/v3/registration5-semver1/$packageName/index.json"
 
 # Call the NuGet API for the package and get the current published version.
 $nugetIndex = Invoke-RestMethod -Uri $url -Method Get
 $currentPublishedVersion = $nugetIndex.items[0].upper
 
-# Convert the published version into a number for comparison e.g. "1.0.9" -> 109
-[int]$currentPublishedVersion = $currentPublishedVersion -replace "\."
+# Convert the published version into a number e.g. "1.0.9" -> 109
+[int]$currentPublishedVersionNumber = $currentPublishedVersion -replace "\."
 
 # Validate that the version number has been updated.
-if ($currentProjectVersion -le $currentPublishedVersion) {
+if ($currentProjectVersionNumber -le $currentPublishedVersionNumber) {
     Write-Error "The project version in versioning.props file ($currentProjectVersion) `
     has not been bumped up. The current published version is $currentPublishedVersion. `
     Please increment the current project version."
