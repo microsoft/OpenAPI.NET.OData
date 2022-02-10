7fd0fa74a811f2b0efab9618f10290d6424367a8
Daniel Mbaluka <danielmbaluka@gmail.com>
2022-02-10 10:41:24 +0300

    clean up release pipeline

--- scripts/ValidateProjectVersionUpdated.ps1
+++ scripts/ValidateProjectVersionUpdated.ps1
@@ -31,14 +31,17 @@ $url = "https://api.nuget.org/v3/registration5-semver1/$packageName/index.json"
 
 # Call the NuGet API for the package and get the current published version.
 $nugetIndex = Invoke-RestMethod -Uri $url -Method Get
-[version]$currentPublishedVersion = $nugetIndex.items[0].upper
+$publishedVersionString = $nugetIndex.items[0].upper
+
+# Cast the published version string to System.Version
+[version]$currentPublishedVersion = $publishedVersionString
 
 # Validate that the version number has been updated.
 if ($currentProjectVersion -le $currentPublishedVersion) {
     Write-Error "The project version in versioning.props file ($projectVersion) `
-    has not been bumped up. The current published version is $currentPublishedVersion. `
+    has not been bumped up. The current published version is $publishedVersionString. `
     Please increment the current project version."
 }
 else {
-    Write-Host "Validated that the version has been updated from $currentPublishedVersion to $currentProjectVersion" -ForegroundColor Green
+    Write-Host "Validated that the version has been updated from $publishedVersionString to $currentProjectVersion" -ForegroundColor Green
 }
