cff7e3ee216ee3081462aac636cc4f301ec0d70c
Irvine Sunday <irochand@microsoft.com>
2022-03-02 00:51:40 +0300

    Add Odata.Core.V1.OptionalParameter

---
+++ src/Microsoft.OpenApi.OData.Reader/Vocabulary/Core/CoreConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.OpenApi.OData.Vocabulary.Core
+{
+    internal class CoreConstants
+    {
+        public const string OptionalParameter = "Org.OData.Core.V1.OptionalParameter";
+    }
+}
---
+++ src/Microsoft.OpenApi.OData.Reader/Vocabulary/Core/OptionalParameterType.cs
@@ -0,0 +1,16 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace Microsoft.OpenApi.OData.Vocabulary.Core
+{
+    /// <summary>
+    /// Complex Type: Org.OData.Core.V1.OptionalParameterType
+    /// </summary>
+    [Term("Org.OData.Core.V1.OptionalParameter")]
+    internal class OptionalParameterType
+    {
+        public string DefaultValue { get; set; }
+    }
+}
