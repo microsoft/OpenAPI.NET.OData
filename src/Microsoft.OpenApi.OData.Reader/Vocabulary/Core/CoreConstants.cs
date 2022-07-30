aa794092c61ed7569c50e59f1ce381cc86641c34
Millicent Achieng <achieng.milli@gmail.com>
2022-07-31 00:17:29 +0300

    Add Core vocabulary constants

---
+++ src/Microsoft.OpenApi.OData.Reader/Vocabulary/Core/CoreConstants.cs
@@ -0,0 +1,23 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -----------------------------------------------------------
+
+namespace Microsoft.OpenApi.OData.Vocabulary.Core
+{
+    /// <summary>
+    /// Term names of the Core vocabulary
+    /// </summary>
+    internal class CoreConstants
+    {
+        /// <summary>
+        /// Org.OData.Core.V1.Links
+        /// </summary>
+        public const string Links = "Org.OData.Core.V1.Links";
+
+        /// <summary>
+        /// Org.OData.Core.V1.Revisions
+        /// </summary>
+        public const string Revisions = "Org.OData.Core.V1.Revisions";
+    }
+}
