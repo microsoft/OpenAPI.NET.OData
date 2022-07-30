305b2de5e82b63a536aff924806d0d9dc2ea131a
Millicent Achieng <achieng.milli@gmail.com>
2022-07-30 23:21:33 +0300

    Add Links to Core vocabularies

---
+++ src/Microsoft.OpenApi.OData.Reader/Vocabulary/Core/LinksType.cs
@@ -0,0 +1,39 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// ------------------------------------------------------------
+
+using Microsoft.OData.Edm.Vocabularies;
+using Microsoft.OpenApi.OData.Common;
+using Microsoft.OpenApi.OData.Edm;
+
+namespace Microsoft.OpenApi.OData.Vocabulary.Core
+{
+    /// <summary>
+    /// Complex Type: Org.OData.Core.V1.Links
+    /// </summary>
+    [Term("Org.OData.Core.V1.Links")]
+    internal class LinksType : IRecord
+    {
+        /// <summary>
+        /// The link relation type.
+        /// </summary>
+        public string Rel { get; private set; }
+
+        /// <summary>
+        /// The link to the documentation.
+        /// </summary>
+        public string Href { get; private set; }
+
+        /// <summary>
+        /// Init the <see cref="LinksType"/>.
+        /// </summary>
+        /// <param name="record"></param>
+        public virtual void Initialize(IEdmRecordExpression record)
+        {
+            Utils.CheckArgumentNull(record, nameof(record));
+            Rel = record.GetString("rel");
+            Href = record.GetString("href");
+        }
+    }
+}
