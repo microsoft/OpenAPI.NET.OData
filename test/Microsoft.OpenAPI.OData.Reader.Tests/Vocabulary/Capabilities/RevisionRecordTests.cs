59455f8cb9de6ef1de2acc8894d64ec4eb71407c
Irvine Sunday <40403681+irvinesunday@users.noreply.github.com>
2023-04-17 15:54:25 +0300

    [Fix] Adds support for all `Org.OData.Core.V1.RevisionKind` enum values (#373)
    * Rename classes; remove exception throwing in constructor
    
    * Update test
    
    * Update release notes

--- test/Microsoft.OpenAPI.OData.Reader.Tests/Vocabulary/Capabilities/RevisionRecordTests.cs
+++ test/Microsoft.OpenAPI.OData.Reader.Tests/Vocabulary/Capabilities/RevisionRecordTests.cs
@@ -11,13 +11,13 @@ using Xunit;
 
 namespace Microsoft.OpenApi.OData.Reader.Vocabulary.Capabilities.Tests;
 
-public class DeprecatedRevisionTypeTests
+public class RevisionRecordTests
 {
     [Fact]
     public void DefaultPropertyAsNull()
     {
         // Arrange & Act
-        DeprecatedRevisionsType revision = new();
+        RevisionRecord revision = new();
 
         // Assert
         Assert.Null(revision.Date);
@@ -30,7 +30,7 @@ public class DeprecatedRevisionTypeTests
     public void InitializeWithNullRecordThrows()
     {
         // Arrange & Act
-        DeprecatedRevisionsType revision = new();
+        RevisionRecord revision = new();
 
         // Assert
         Assert.Throws<ArgumentNullException>("record", () => revision.Initialize(record: null));
@@ -50,7 +50,7 @@ public class DeprecatedRevisionTypeTests
             new EdmPropertyConstructor("Version", new EdmStringConstant("2021-05/test")));
 
         // Act
-        DeprecatedRevisionsType revision = new();
+        RevisionRecord revision = new();
         revision.Initialize(record);
 
         // Assert
@@ -67,20 +67,32 @@ public class DeprecatedRevisionTypeTests
         Assert.Equal(new DateTime(2021, 10, 24), revision.RemovalDate);
     }
     [Fact]
-    public void ThrowsOnWrongKind()
+    public void WorksForAllKinds()
     {
         // Arrange
-        IEdmRecordExpression record = new EdmRecordExpression(
+        IEdmRecordExpression record1 = new EdmRecordExpression(
             new EdmPropertyConstructor("Date", new EdmDateConstant(new Date(2021, 8, 24))),
             new EdmPropertyConstructor("RemovalDate", new EdmDateConstant(new Date(2021, 10, 24))),
             new EdmPropertyConstructor("Kind", new EdmEnumMemberExpression(addedValue)), 
             new EdmPropertyConstructor("Description", new EdmStringConstant("The identityProvider API is deprecated and will stop returning data on March 2023. Please use the new identityProviderBase API.")),
             new EdmPropertyConstructor("Version", new EdmStringConstant("2021-05/test")));
 
+        IEdmRecordExpression record2 = new EdmRecordExpression(
+            new EdmPropertyConstructor("Date", new EdmDateConstant(new Date(2023, 3, 2))),
+            new EdmPropertyConstructor("RemovalDate", new EdmDateConstant(new Date(2023, 05, 30))),
+            new EdmPropertyConstructor("Kind", new EdmEnumMemberExpression(deprecatedValue)),
+            new EdmPropertyConstructor("Description", new EdmStringConstant("Private preview test.")),
+            new EdmPropertyConstructor("Version", new EdmStringConstant("2023-03/test")));
+
         // Act
-        DeprecatedRevisionsType revision = new();
+        RevisionRecord revision1 = new();
+        revision1.Initialize(record1);
+        
+        RevisionRecord revision2 = new();
+        revision2.Initialize(record2);
 
         // Assert
-        Assert.Throws<InvalidOperationException>(() => revision.Initialize(record));
+        Assert.Equal(RevisionKind.Added, revision1.Kind);
+        Assert.Equal(RevisionKind.Deprecated, revision2.Kind);
     }
 }
