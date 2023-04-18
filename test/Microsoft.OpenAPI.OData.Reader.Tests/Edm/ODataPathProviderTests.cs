cc66bf923bd63d8e5c32539f104a9fd6174c923f
Irvine Sunday <irochand@microsoft.com>
2023-04-18 17:39:58 +0300

    Update tests and integration file

--- test/Microsoft.OpenAPI.OData.Reader.Tests/Edm/ODataPathProviderTests.cs
+++ test/Microsoft.OpenAPI.OData.Reader.Tests/Edm/ODataPathProviderTests.cs
@@ -52,7 +52,7 @@ namespace Microsoft.OpenApi.OData.Edm.Tests
 
             // Assert
             Assert.NotNull(paths);
-            Assert.Equal(18054, paths.Count());
+            Assert.Equal(18050, paths.Count());
             AssertGraphBetaModelPaths(paths);
         }
 
@@ -64,6 +64,9 @@ namespace Microsoft.OpenApi.OData.Edm.Tests
 
             // Test that $value segments are created for entity types with base types with HasStream="true"
             Assert.NotNull(paths.FirstOrDefault(p => p.GetPathItemName().Equals("/me/chats({id})/messages({id1})/hostedContents({id2})/$value")));
+
+            // Test that count restrictions annotations for navigation properties work
+            Assert.Null(paths.FirstOrDefault(p => p.GetPathItemName().Equals("/me/drives/$count")));
         }
 
         [Fact]
@@ -84,7 +87,7 @@ namespace Microsoft.OpenApi.OData.Edm.Tests
 
             // Assert
             Assert.NotNull(paths);
-            Assert.Equal(18705, paths.Count());
+            Assert.Equal(18701, paths.Count());
         }
 
         [Fact]
