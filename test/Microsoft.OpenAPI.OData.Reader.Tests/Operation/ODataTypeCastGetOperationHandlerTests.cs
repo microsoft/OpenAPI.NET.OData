cd27a72e18ef516e26c07202adc2ae9684a6f4ac
Vincent Biret <vibiret@microsoft.com>
2024-12-30 12:13:43 -0500

    fix: NRT on properties
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- test/Microsoft.OpenAPI.OData.Reader.Tests/Operation/ODataTypeCastGetOperationHandlerTests.cs
+++ test/Microsoft.OpenAPI.OData.Reader.Tests/Operation/ODataTypeCastGetOperationHandlerTests.cs
@@ -137,7 +137,7 @@ public class ODataTypeCastGetOperationHandlerTests
         {
             Assert.Null(operation.OperationId);
         }
-        Assert.False(operation.Responses["200"].Content["application/json"].Schema.Properties.ContainsKey("value"));
+        Assert.False(operation.Responses["200"].Content["application/json"].Schema.Properties?.ContainsKey("value") ?? false);
     }
     [Theory]
     [InlineData(true, true)]
